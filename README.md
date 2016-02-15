e493e612b406f46bbbf865e02782dd1d3ce5817e
Shahid <shahid@codeoforgeek>
2016-02-15 12:54:52 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Google recaptcha demo
+
+Tutorial > https://codeforgeek.com/2014/12/google-recaptcha-tutorial/
+
+Demo > http://demo.codeforgeek.com/google-captcha/
---
+++ form.php
@@ -0,0 +1,24 @@
+<?php
+
+        $email;$comment;$captcha;
+        if(isset($_POST['email'])){
+          $email=$_POST['email'];
+        }if(isset($_POST['comment'])){
+          $email=$_POST['comment'];
+        }if(isset($_POST['g-recaptcha-response'])){
+          $captcha=$_POST['g-recaptcha-response'];
+        }
+        if(!$captcha){
+          echo '<h2>Please check the the captcha form.</h2>';
+          exit;
+        }
+	$secretKey = "";
+	$ip = $_SERVER['REMOTE_ADDR'];
+        $response=file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=".$secretKey."&response=".$captcha."&remoteip=".$ip);
+	$responseKeys = json_decode($response,true);
+        if(intval($responseKeys["success"]) !== 1) {
+          echo '<h2>You are spammer ! Get the @$%K out</h2>';
+        } else {
+          echo '<h2>Thanks for posting comment.</h2>';
+        }
+?>
