84d3ee984d50319b8f6c1e32ae0229e8e54619ab
root <shahid.s@zebpay.com.com>
2019-01-31 16:45:58 +0530

    v2 v3 update

--- form_v3.php
+++ form_v3.php
@@ -5,8 +5,8 @@
           $email=$_POST['email'];
         }if(isset($_POST['comment'])){
           $comment=$_POST['comment'];
-        }if(isset($_POST['g-recaptcha-response'])){
-          $captcha=$_POST['g-recaptcha-response'];
+        }if(isset($_POST['token'])){
+          $captcha=$_POST['token'];
 	  }
         if(!$captcha){
           echo '<h2>Please check the the captcha form.</h2>';
@@ -16,12 +16,14 @@
 	$ip = $_SERVER['REMOTE_ADDR'];
 
 	// post request to server
+
 	$url =  'https://www.google.com/recaptcha/api/siteverify?secret=' . urlencode($secretKey) .  '&response=' . urlencode($captcha);
 	$response = file_get_contents($url);
 	$responseKeys = json_decode($response,true);
+	header('Content-type: application/json');
 	if($responseKeys["success"]) {
-		echo '<h2>Thanks for posting comment</h2>';
+		echo json_encode(array('success' => 'true'));
 	} else {
-		echo '<h2>You are spammer ! Get the @$%K out</h2>';
+		echo json_encode(array('success' => 'false'));
         }
 ?>
